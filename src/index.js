773538100afa97b455b3799298d95825dfb5a234
Justin <justinrkinsman@gmail.com>
2022-12-06 11:44:05 -0400

    Add URIs to routes

--- src/index.js
+++ src/index.js
@@ -6,8 +6,32 @@ const app = express()
 
 app.use(cors())
 
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
 app.get('/', (req, res) => {
-    return res.send('Received a GET HTTP method')
+    return res.redirect('/users')
 })
 
 app.post('/', (req, res) => {
@@ -23,19 +47,27 @@ app.delete('/', (req, res) => {
 })
 
 app.get('/users', (req, res) => {
-    return res.send('GET HTTP method on a user resource')
+    return res.send(Object.values(users))
 })
 
 app.post('/users', (req, res) => {
     return res.send('POST HTTP method on a user resource')
 })
 
-app.put('/users/:userId', (req, res) => {
-    return res.send(`PUT HTTP method on user/${req.params.userId} resource`)
+app.get('/users/:userId', (req, res) => {
+    return res.send(res.send(users[req.params.userId]))
 })
 
 app.delete('/users/:userId', (req, res) => {
     return res.send(`DELETE HTTP method on user/${req.params.userId} resource`)
+})
+
+app.get('/messages', (req, res) => {
+    return res.send(Object.values(messages))
+})
+
+app.get('/messages/:messageId', (req, res) => {
+    return res.send(messages[req.params.messageId])
 })
 
 app.listen(3000, () => 
