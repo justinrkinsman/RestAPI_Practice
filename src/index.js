52dd9cd67011d41e2ca1641107e0e6bd0bd67b44
Justin <justinrkinsman@gmail.com>
2022-12-06 11:29:20 -0400

    Add rest resources to routes

--- src/index.js
+++ src/index.js
@@ -22,6 +22,22 @@ app.delete('/', (req, res) => {
     return res.send('Received a DELETE HTTP method')
 })
 
+app.get('/users', (req, res) => {
+    return res.send('GET HTTP method on a user resource')
+})
+
+app.post('/users', (req, res) => {
+    return res.send('POST HTTP method on a user resource')
+})
+
+app.put('/users/:userId', (req, res) => {
+    return res.send(`PUT HTTP method on user/${req.params.userId} resource`)
+})
+
+app.delete('/users/:userId', (req, res) => {
+    return res.send(`DELETE HTTP method on user/${req.params.userId} resource`)
+})
+
 app.listen(3000, () => 
     console.log(`Example app listening on post ${process.env.PORT}!`)
 )
