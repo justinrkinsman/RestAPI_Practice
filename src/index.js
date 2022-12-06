eb5e578f462def56ac0af6987e6a6975a7bfa686
Justin <justinrkinsman@gmail.com>
2022-12-06 11:52:34 -0400

    Add post message route to REST API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import { v4 as uuidv4 } from 'uuid'
 import 'dotenv/config'
 import cors from 'cors'
 import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
 let users = {
@@ -34,24 +37,8 @@ app.get('/', (req, res) => {
     return res.redirect('/users')
 })
 
-app.post('/', (req, res) => {
-    return res.send('Received a POST HTTP method')
-})
-
-app.put('/', (req, res) => {
-    return res.send('Received a PUT HTTP method')
-})
-
-app.delete('/', (req, res) => {
-    return res.send('Received a DELETE HTTP method')
-})
-
 app.get('/users', (req, res) => {
     return res.send(Object.values(users))
-})
-
-app.post('/users', (req, res) => {
-    return res.send('POST HTTP method on a user resource')
 })
 
 app.get('/users/:userId', (req, res) => {
@@ -64,6 +51,18 @@ app.delete('/users/:userId', (req, res) => {
 
 app.get('/messages', (req, res) => {
     return res.send(Object.values(messages))
+})
+
+app.post('/messages', (req, res) => {
+    const id = uuidv4()
+    const message = {
+        id,
+        text: req.body.text,
+    }
+
+    messages[id] = message
+    
+    return res.send(message)
 })
 
 app.get('/messages/:messageId', (req, res) => {
