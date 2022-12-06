774454dc0655b2cbfa2d9ef5cb80451f61093462
Justin <justinrkinsman@gmail.com>
2022-12-06 10:59:43 -0400

    Install express

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import 'dotenv/config'
+import express from 'express'
 
-console.log("Hello Project!")
+const app = express()
 
-console.log(process.env.MY_SECRET)
+app.listen(3000, () => 
+    console.log('Example app listening on post 3000!')
+)
